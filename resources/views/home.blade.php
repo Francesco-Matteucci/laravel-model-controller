8443f940207f08d933111728c7b611611494b300
Francesco-Matteucci <francescomatteucci31@gmail.com>
2024-10-24 15:52:21 +0200

    exercize done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Benvenuto nella home page</h1>
-    <p>Vai alla lista dei film <a href="/movies">qui</a>.</p>
+<div class="home-content text-center text-dark d-flex justify-content-center align-items-center flex-column">
+    <h1 class="display-3 mb-4">Benvenuto nella nostra Movie App</h1>
+    <p class="lead">Esplora la nostra collezione di film e scopri nuovi titoli.</p>
+    <a href="/movies" class="btn btn-dark btn-lg mt-3">Vai alla lista dei film</a>
+</div>
 @endsection
--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
     <div class="container">
-        <h1 class="my-4 text-center">Lista dei Film</h1>
-        <div class="row">
+        <div class="movie-content row p-3">
+            <h1 class="my-4 text-center">Lista dei Film</h1>
             @foreach ($movies as $movie)
                 <div class="col-md-4">
-                    <div class="card mb-4 shadow-sm">
+                    <div class="card mb-4 bg-dark-subtle">
                         <div class="card-body">
                             <h5 class="card-title">{{ $movie->title }}</h5>
                             <p class="card-text">Titolo Originale: {{ $movie->original_title }}</p>
