d4f64cacac83738400f049c368da83f0b095b40b
Francesco-Matteucci <francescomatteucci31@gmail.com>
2024-10-24 14:05:45 +0200

    Added home route and movie list route

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Benvenuto nella home page</h1>
+    <p>Vai alla lista dei film <a href="/movies">qui</a>.</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
+// ROUTE Home page
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+// ROUTE lista Film
+Route::get('/movies', [MovieController::class, 'index']);
